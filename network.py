71af92713864cdc2a8b12a45290b954c522e36cd
nandhakishorecs <nandhakishorecs@gmail.com>
2025-03-01 22:36:14 +0530

    added skelton file for neural networks

---
+++ network.py
@@ -0,0 +1,14 @@
+import numpy as np                  
+import wandb
+
+from initialisers import * 
+from activations import * 
+from loss_functions import *
+from preprocessing import * 
+
+# ------------------- A Complete Neural Network -------------------
+#   Author: Nandhakishore C S 
+#   Roll Number: DA24M011
+#   Submitted as part of DA6401 Introduction to Deep Learning Assignment 1
+# --------------------------------------------------------------------------
+
