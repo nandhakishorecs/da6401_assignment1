f07c649d68fcbd7b712720fa377f13d4c93fbabd
nandhakishorecs <nandhakishorecs@gmail.com>
2025-03-02 01:07:04 +0530

    added layers: Input and Dense

---
+++ layers.py
@@ -0,0 +1,41 @@
+import numpy as np                  # for vector / matrix operations
+from activations import *           # functions sigmoid, tanh, relu, softmax 
+
+# ------------------- Layers for Neural Networks -------------------
+#   Author: Nandhakishore C S 
+#   Roll Number: DA24M011
+#   Submitted as part of DA6401 Introduction to Deep Learning Assignment 1
+# --------------------------------------------------------------------------
+
+activation_functions_map = {
+    'Sigmoid': sigmoid(), 
+    'Tanh': tanh(), 
+    'ReLU': relu(), 
+    'Softmax': softmax()
+}
+
+# We have K layers - Kth layer is output, 1st layer is Input and we have L-1 Hidden layers 
+# Input Layer 
+class Input: 
+    __slots__ = '_name', '_input', '_size', '_a'
+    def __init__(self, X:np.ndarray) -> None:
+        self._name = 'Input',
+        self._input = X, 
+        self._size = self._input.shape[0]
+        self._a = self._input                       # variable purpose unknown
+
+    # python special function to give a string representavle name to the class
+    def __repr__(self) -> str: 
+        return  self.__class__.__name__ + 'of size:' + str(self._size)
+
+# Hidden Layers 
+class Dense: 
+    __slots__ = '_size', '_activation_func', '_activation_func_name', '_name'
+    def __init__(self, size:int, activation: str, name: str, isLastLayer:bool = False) -> None:
+        self._name = name 
+        self._size = size 
+        self._activation_func = activation_functions_map[activation]
+        self._activation_func_name = activation
+
+    def __repr__(self) -> str: 
+        return self.__class__.__name__ + 'of size:' + str(self._size) + '|' + self._activation_func_name + 'activation'
--- network.py
+++ network.py
@@ -1,10 +1,15 @@
+import math 
 import numpy as np                  
 import wandb
+from tqdm import tqdm 
+from copy import deepcopy
 
 from initialisers import * 
 from activations import * 
 from loss_functions import *
 from preprocessing import * 
+from optimisers import * 
+from layers import *
 
 # ------------------- A Complete Neural Network -------------------
 #   Author: Nandhakishore C S 
@@ -12,3 +17,19 @@ from preprocessing import *
 #   Submitted as part of DA6401 Introduction to Deep Learning Assignment 1
 # --------------------------------------------------------------------------
 
+# Basic skeleton - yet to be implemented
+class NeuralNetwork: 
+    def __init__(
+            self, 
+            layers: list, 
+            batch_size: int, 
+            optimiser: str, 
+            initilaisation: str, 
+            epochs: int, 
+            target: np.ndarray, 
+            loss_function: str, 
+            validation_features: np.ndarray, 
+            validation_target: np.ndarray, 
+            optimiser_parameters = None
+        ) -> None:
+        pass
